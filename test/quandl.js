6d4430d81bca3032a90c565ef25efa1ef511e291
Victory <git@dfhu.org>
2016-01-17 15:18:45 -0500

    add support for last uri call, and show failing test for CBOE/VXEEM

--- test/quandl.js
+++ test/quandl.js
@@ -41,4 +41,27 @@ describe("quandl", function(){
         });
 
     });
+
+
+    describe("contains last_uri_call", function(){
+        it("contains an empty string value", function() {
+            assert.equal(quandl.last_uri_called, "");
+        });
+
+        it("has correct api call for v3", function() {
+            quandl.configure({
+                api_version: 3,
+                auth_token: "dsahFHUiewjjd"
+            });
+
+            quandl.dataset(
+                {source: 'CBOE/VXEEM', format: 'json'},
+                function(){});
+
+            assert.equal(
+                quandl.last_uri_called, 
+                "api/v1/datasets/CBOE/VXEEM.json");
+        });
+    });
+
 });
